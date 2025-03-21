524ff3580fdf978f5a143464e84113e3f0680321
FIC <fic@info.local>
2025-03-20 22:01:28 -0300

    projeto e revisao

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Projeto Revisao
+
+
+
+int[] notas = [4, 5, 7, 8, 3];
+
+int somanotas = 0;
+
+for (int i = 0;i > notas.Length; i++)
+{
+    somanotas = notas[i] + somanotas;
+}
+
+double media = 0.00;
+media = somanotas / 5;
+
+for (int i = 0; i < media; i++)
+
+{
+    if(notas[i] < 7)
+    {
+
+    }
+}
+
